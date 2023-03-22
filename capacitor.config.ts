67165bf250660f89c6d5f5892062d44a8176b695
hnguyen48206 <hoangbich1989@gmail.com>
2023-03-22 10:03:03 +0700

    icon and splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,19 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ivms.vn',
   appName: 'ivms-mobile',
-  webDir: 'dist',
+  webDir: 'dist/ivms_mobile',
   bundledWebRuntime: false,
-  server:{
-    cleartext:true
+  server: {
+    cleartext: true
+  },
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 3000,
+      "launchAutoHide": true,
+      "androidScaleType": "CENTER_CROP",
+      "splashImmersive": true,
+      "backgroundColor": "#ffffff"
+    }
   }
 };
 
